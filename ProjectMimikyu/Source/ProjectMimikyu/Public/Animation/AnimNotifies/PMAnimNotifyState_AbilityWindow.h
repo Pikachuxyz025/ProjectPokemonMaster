8a0b1bd163d1fc5afdce59c06119b4c274c849bf
Zerlok <1351davis@gmail.com>
2026-07-12 16:18:26 -0400

    PMAnimNotifyState_AbilityWindow

---
+++ ProjectMimikyu/Source/ProjectMimikyu/Public/Animation/AnimNotifies/PMAnimNotifyState_AbilityWindow.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "PMAnimNotifyState_AbilityWindow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMIMIKYU_API UPMAnimNotifyState_AbilityWindow : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+};
